82a42cebe11807a178e10cbf0f0793762a567575
Krishna Suravarapu <36037520+KrishnaSuravarapu@users.noreply.github.com>
2025-05-05 14:32:17 +0530

    Chore: Removing spec pattern

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,6 @@ module.exports = defineConfig({
   headless: false,
   e2e: {
     baseUrl: "https://example.cypress.io",
-    specPattern: "cypress/e2e/1-getting-started/*.cy.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     }
